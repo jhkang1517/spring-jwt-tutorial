7c621e1d1df2f881b475fdb7796ef8e8e3ae47d9
jhkang1517 <jhkang1517@gmail.com>
2022-12-05 21:52:43 +0900

    initial commit
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

---
+++ src/main/java/com/blackswitch/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.blackswitch.jwttutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll();
+        return http.build();
+    }
+}
